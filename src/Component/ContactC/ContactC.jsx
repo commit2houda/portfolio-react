afc2dbdc4969b91987a49f79eb6a52df4adb0b4d
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-10-07 14:01:45 +0100

    done

--- src/Component/ContactC/ContactC.jsx
+++ src/Component/ContactC/ContactC.jsx
@@ -1,35 +1,56 @@
-import { Component } from "react";
+// import { Component } from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./ContactC.css";
-import Aos from "aos";
-import "aos/dist/aos.css";
+// import Aos from "aos";
+// import "aos/dist/aos.css";
 
-class ContactC extends Component {
-  componentDidMount() {
+const ContactC = () => {
+  /*  componentDidMount() {
     Aos.init({ duration: 2000 });
-  }
-  render() {
-    return (
-      <div className="contactMain" id="contactS" data-aos="flip-left">
-        <div className="contactTitles">
-          <h5>03. What's Next ?</h5>
-          <h1>Get In Touch</h1>
-          <p>
-            Although I'm not currently looking for any new opportunities, my
-            inbox is always open. Whether you have a question or just want to
-            say hi, I'll try my best to get back to you!
-          </p>
-        </div>
-        <form autocomplete="off">
-          <div className="namendEmail">
-            <input type="text" placeholder="Name" id="name" />
-            <input type="email" placeholder="email" id="email" />
-          </div>
-          <input type="text" placeholder="Subject" id="subject" />
-          <input type="text" placeholder="Message" id="msg" />
-          <button type="submit">Send Message</button>
-        </form>
+  } */
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="contactMain" id="contactS" /* data-aos="flip-left" */>
+      <div className="contactTitles">
+        <h5>03. What's Next ?</h5>
+        <h1>Get In Touch</h1>
+        <p>
+          Although I'm not currently looking for any new opportunities, my inbox
+          is always open. Whether you have a question or just want to say hi,
+          I'll try my best to get back to you!
+        </p>
       </div>
-    );
-  }
-}
+      <form autocomplete="off" ref={form} onSubmit={sendEmail}>
+        <div className="namendEmail">
+          <input type="text" placeholder="Name" id="name" name="from_name" />
+          <input type="email" placeholder="email" id="email" />
+        </div>
+        <input type="text" placeholder="Subject" id="subject" />
+        <textarea type="text" placeholder="Message" id="msg" name="message" />
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  );
+};
+
 export default ContactC;
--- src/Component/SocialMedia/SocialMedia.jsx
+++ src/Component/SocialMedia/SocialMedia.jsx
@@ -14,22 +14,22 @@ class SocialMedia extends Component {
       <div className="mainicons">
         <ul className="icons">
           <li>
-            <a href="https://github.com/HoudaElbaidak6" target="_blank">
+            <a href="https://github.com/HoudaElbaidak6" target="_blank" className="icon-size">
               <FontAwesomeIcon className="icon-size" icon={faGithub} />
             </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/houda-el-baidak-4b66a4237/" target="_blank">
+            <a href="https://www.linkedin.com/in/houda-el-baidak-4b66a4237/" target="_blank" className="icon-size">
               <FontAwesomeIcon className="icon-size" icon={faLinkedin} />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="#" className="icon-size">
               <FontAwesomeIcon className="icon-size" icon={faInstagram} />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="#" className="icon-size">
               <FontAwesomeIcon className="icon-size" icon={faTwitter} />
             </a>
           </li>
